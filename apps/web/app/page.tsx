f664d6284b2ada4e55c48a886ccfc76b8f63f320
shaurya9935 <prakhargupta9935@gmail.com>
2026-06-18 03:27:53 +0530

    register api complete

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { trpc } from "~/trpc/client";
-
 export default function Home() {
- const { data } = trpc.chaicode.useQuery({email: 'p@e.com', name: 'Shaurya', age: 20})
+
   return (
     <main className="min-h-screen min-w-screen flex justify-center items-center">
       <div>
-        <h2>Server Message: {data?.message}</h2>
+
       </div>
     </main>
   );
